e0edba86b5a641d2795eeb3b5c985da384dd6c0e
Arhip Storozhev <49954420+arsestor@users.noreply.github.com>
2023-04-29 22:53:43 +0500

    update file

--- game_model.py
+++ game_model.py
@@ -1,5 +1,11 @@
 from random import randbytes, randint
 
+def check_players(a, b):
+    if b.accept_num(a.send_num()) == a.accept_num(b.send_num()):
+        return True
+    else:
+        return False
+
 class Player():
     def __init__(self):
         self.player_num = hash(randbytes(32))
@@ -9,19 +15,8 @@ class Player():
     
     def send_num(self):
         return self.player_num
+    
+
 
 a = Player()
 b = Player()
-if b.accept_num(a.send_num()) == a.accept_num(b.send_num()):
-    print(True)
-
-# def get_player_hash():
-#     return hash(randbytes(32))
-
-# def check(x, y):
-#     if hash(x*hash(y)) == hash(hash(x)*y):
-#         return True
-#     else:
-#         return False
-
-# print(check(get_player_hash(), get_player_hash()))
