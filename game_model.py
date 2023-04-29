69735a7acae96c4c02f03f83f31bc5faa1aa84d3
Arhip Storozhev <49954420+arsestor@users.noreply.github.com>
2023-04-30 00:02:33 +0500

    day #1 ended

--- game_model.py
+++ game_model.py
@@ -1,22 +1,19 @@
 from random import randbytes, randint, choice
 
 def check_players(a, b):
-    if b.accept_num(a.send_num()) == a.accept_num(b.send_num()):
-        return [True, a.accept_num(b.send_num())]
+    if b.accept_num(a.player_num) == a.accept_num(b.player_num):
+        return [True, a.accept_num(b.player_num)]
     else:
-        return [False, a.accept_num(b.send_num()), b.accept_num(a.send_num())]
+        return [False, a.accept_num(b.player_num), b.accept_num(a.player_num)]
 
 class Player():
     def __init__(self):
-        self.player_num = choice(range(0, 1001))
+        self.player_num = choice(range(1, 101))
 
     def accept_num(self, other_num):
         return self.player_num * other_num
-    
-    def send_num(self):
-        return self.player_num
-    
 
+# ♠ ♣ ♥ ♦
 
 a = Player()
 b = Player()
