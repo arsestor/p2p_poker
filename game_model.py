226552507dc4a27a629e1bc83fd37d3333399e15
Arhip Storozhev <49954420+arsestor@users.noreply.github.com>
2023-04-29 23:07:35 +0500

    update

--- game_model.py
+++ game_model.py
@@ -1,14 +1,14 @@
-from random import randbytes, randint
+from random import randbytes, randint, choice
 
 def check_players(a, b):
     if b.accept_num(a.send_num()) == a.accept_num(b.send_num()):
-        return True
+        return [True, a.accept_num(b.send_num())]
     else:
-        return False
+        return [False, a.accept_num(b.send_num()), b.accept_num(a.send_num())]
 
 class Player():
     def __init__(self):
-        self.player_num = hash(randbytes(32))
+        self.player_num = choice(range(0, 1001))
 
     def accept_num(self, other_num):
         return self.player_num * other_num
@@ -20,3 +20,8 @@ class Player():
 
 a = Player()
 b = Player()
+ch_p = check_players(a, b)
+if ch_p[0] == True:
+    print(ch_p[1])
+else:
+    print('Игроки не прошли проверку')
